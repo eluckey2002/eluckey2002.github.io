f6cbdda108ba252f1aec0104596e60fd1c04c3f3
eluckey2002 <33357158+eluckey2002@users.noreply.github.com>
2022-04-13 20:04:43 -0700

    reverting

--- src/launchPad.js
+++ src/launchPad.js
@@ -1,74 +1,74 @@
 export default class evanPlugEvanOnEnter extends BasePlugin  {
 
     /** Plugin info */
-    static get id()             { return 'ep_launchpadd' }
-    static get name()           { return 'ep-LaunchPad' }
+    static get id()             { return 'evanplug-CompEventMessaging' }
+    static get name()           { return 'evanPlug-IDCompEventMessaging' }
     static get description()    { return 'custom component enter exit' }
 
     /** Called when the plugin is loaded */
     onLoad() {
 
-       console.log("ComponentCompEventMessaging Plug loaded - v0.7")
+       console.log("ComponentCompEventMessaging Plug loaded - v0.1")
        
         // Register component
-        this.objects.registerComponent(evanPlugLaunchPadBase, {
-            id: 'evanplug-launch-bases',
-            name: 'ep-launchpad',
-            description: "Launch a user in the air in a specified direction",
+        this.objects.registerComponent(evanPlugVelocityBase, {
+            id: 'evanplug-velocity-base',
+            name: 'Evan Plugin Testing',
+            description: "Testing plugin capabilities",
             settings: [
-                { id: 'lbl-xdist', type: 'label', value: "X Dist" },
-                { id: 'xdist', type: 'number',  default: '5'},
-                { id: 'lblydist', type: 'label', value: "Y (UP) Dist" },
-                { id: 'ydist', type: 'number',  default: '5'},
-                { id: 'lblzdist', type: 'label', value: "Z Dist" },
-                { id: 'zdist', type: 'number',  default: '5'},
-
-               
+                { type: 'label', value: "onEnter Message" },
+                { id: 'txt-onEnter', type: 'text',  value: 'You have an entered a presentation sound zone. You can hear and speak to everyone throughout the whole zone. Sound is no longer limited by distance.'},
+                { type: 'label', value: "onExit Message" },
+                { id: 'txt-onExit', type: 'text',  value: 'You have left the presentation sound zone.  You will only be able to speak and hear others who are close to you.'}
             ]
         })
 
     }
 }
 
-class evanPlugLaunchPadBase extends BaseComponent {
-    isTriggered = false
-    instanceID = "string"
+class evanPlugVelocityBase extends BaseComponent {
+    hasTriggered = false
+    isPreviousInside = false
+    onEnterMessage = ""
+    onExitMessage = ""
 
     onLoad(){
-       
+        console.log("Event Component Loaded 0.5v")
 
-        //Generate instanceID
+            // Generate instance ID
+        //this.instanceID = Math.random().toString(36).substr(2)
+
+        this.timer = setInterval(this.checkIfWithin.bind(this), 1000)
+
     
-        this.instanceID = Math.random().toString(36)
 
-        console.log("LaunchPad BaseComp Loaded Instance ID is")
-        console.log(this.instanceID)
 
-        this.timer = setInterval(this.onTimer.bind(this), 1000)
-
-   
     }
 
-  
-
     onUnload(){
-          console.log("Launchpad Plugin Base unloaded")
+            console.log("on unload method called")
+
             clearInterval(this.Timer)
 
 
     }
 
 
-  
+    onSettingsUpdated(field, value){
+            //called when field is updated
+
+            console.log(field + ' changed to ' + value)
+
+
+    }
+
+   
     async onTimer() {
-     
 
         // Only allow triggering once
-        if (this.isTriggered) {
+        if (this.hasTriggered) {
             return
         }
-
-    
 
         // Get user position
         let userPos = await this.plugin.user.getPosition()
@@ -83,46 +83,86 @@ class evanPlugLaunchPadBase extends BaseComponent {
         // Calculate distance between the user and this pickup
         const distance = Math.sqrt((x - userPos.x) ** 2 + (y - userPos.y) ** 2 + (z - userPos.z) ** 2)
 
-        // If close enough
+        // If close enough, trigger Mine
         let triggerDistance = 1
         if (distance < triggerDistance) {
-
-            this.onTrigger()
+            this.onEnter()
             return
-        
-                  
-        }      
-     
-      
+        }       
 
     }
 
-    async onTrigger()
-    {
-        this.isTriggered = true
+   async onEnter(){
+        this.hasTriggered = true
+        console.log("onEnter called!")
 
          // Get user position
          let userPos = await this.plugin.user.getPosition()
 
-         //set position in air
-         await this.plugin.user.setPosition(userPos.x + parseInt(this.getField('xdist')), userPos.y + parseInt(this.getField('ydist')), userPos.z + parseInt(this.getField('zdist')),false)   
-        
-
-         //display toast
+            //display toast
         this.plugin.menus.toast({     
-           text: 'You are flying!!! Weeeeeeee!',
-           textColor: '#2DCA8C',
-           duration: 3000})
+            text: 'You are flying!!! Weeeeeeee!',
+            textColor: '#2DCA8C',
+            duration: 5000})
 
-
-        this.isTriggered = false
+         //set position in air
+       await this.plugin.user.setPosition(userPos.x+2, userPos.y + 20, userPos.z+2, false)   
         
+     
 
+        this.hasTriggered = false
+   
+    
     }
 
-   
+   async checkIfWithin(){
+
+         // Get user position
+         let userPos = await this.plugin.user.getPosition()
+
+         // Check if we are inside this object
+         let minX = this.fields.world_center_x - this.fields.world_bounds_x/2
+         let minY = this.fields.world_center_y - this.fields.world_bounds_y/2
+         let minZ = this.fields.world_center_z - this.fields.world_bounds_z/2
+         let maxX = this.fields.world_center_x + this.fields.world_bounds_x/2
+         let maxY = this.fields.world_center_y + this.fields.world_bounds_y/2
+         let maxZ = this.fields.world_center_z + this.fields.world_bounds_z/2
+         let isNowInside = userPos.x >= minX && userPos.x <= maxX && userPos.y >= minY && userPos.y <= maxY && userPos.z >= minZ && userPos.z <= maxZ
+        
+
+    
+         if (!this.isPreviousInside && isNowInside) //outside and now inside
+         {
+                //user has entered
+                this.isPreviousInside = true
+                
+                //display toast
+                this.plugin.menus.toast({     
+                text: 'You have an entered a presentation sound zone. You can hear and speak to everyone throughout the whole zone. Sound is no longer limited by distance.',
+                textColor: '#2DCA8C',
+                duration: 5000})
+
+                
 
 
+
+         }
+
+         if( this.isPreviousInside && !isNowInside) //inside and now outside
+         {
+                //user has exited
+                this.isPreviousInside = false
+               
+                //display toast
+                this.plugin.menus.toast({     
+                    text: 'You have left the presentation sound zone.  You will only be able to speak and hear others who are close to you.',
+                    textColor: '#2DCA8C',
+                    duration: 5000})
+
+
+         }
+           
+    }
    
  
 }
