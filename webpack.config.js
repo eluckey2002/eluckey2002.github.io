b9ef3b2ea6499091c87296e1d7209ec46c0af8e5
eluckey2002 <33357158+eluckey2002@users.noreply.github.com>
2022-04-15 18:08:45 -0700

    ifrsamebas

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path')
 const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
-    entry: './src/iframeMessaging.js',
+    entry: './src/index.js',
     mode: 'production',
     // mode: 'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'iframeoverlay.js',
+        filename: 'iframebs.js',
         library: {
             name: 'module.exports',
             type: 'assign',
